4da7111b0561b69139d44ffb5dc380922ea6dcaa
CHIWHI, FNEG <55752357+chiwei82@users.noreply.github.com>
2024-09-23 20:23:12 +0800

    Update main.py

--- project/fastapi/main.py
+++ project/fastapi/main.py
@@ -2,9 +2,24 @@ import requests
 from fastapi import FastAPI
 import pandas as pd
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from datetime import datetime, timedelta
+import os
+# 使用 os.path.join 構建 static 目錄的路徑
+current_dir = os.path.dirname(os.path.abspath(__file__))  # 當前文件的目錄
+static_dir = os.path.join(current_dir, "static")  # 靜態文件的路徑
 
+# 掛載靜態文件目錄
 app = FastAPI()
+try:
+    app.mount("/", StaticFiles(directory="static", html=True), name="static")
+except:
+    try:
+        app.mount("/static", StaticFiles(directory=static_dir), name="static")
+    except:
+        @app.get("/")
+        async def read_index():
+            return FileResponse(os.path.join(static_dir, "index.html"))
 
 # 從新的 API URL 獲取自行車站即時資料
 @app.get("/bike-stations")
@@ -23,6 +38,3 @@ def get_bike_stations():
 @app.get("/time")
 def get_time():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
-
