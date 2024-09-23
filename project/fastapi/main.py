4906714e29fb6e23e4368ad3fd3f695f9e5eb313
CHIWHI, FNEG <55752357+chiwei82@users.noreply.github.com>
2024-09-23 19:36:39 +0800

    new file added

--- project/fastapi/main.py
+++ project/fastapi/main.py
@@ -1,72 +1,28 @@
-from fastapi import FastAPI,HTTPException
-import asyncio
-from pydantic import BaseModel, EmailStr
+import requests
+from fastapi import FastAPI
+import pandas as pd
+from fastapi.staticfiles import StaticFiles
+from datetime import datetime, timedelta
 
 app = FastAPI()
 
-# http://127.0.0.1:8000/docs# UI
+# 從新的 API URL 獲取自行車站即時資料
+@app.get("/bike-stations")
+def get_bike_stations():
+    url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+    response = requests.get(url)
+    data = response.json()
 
-# CRUD Operation
-# post -- Create
-# get -- Read
-# put -- Update
-# delete -- Delete
-class User(BaseModel):
-    id: int
-    name: str
-    email: EmailStr  # 使用 EmailStr 進行 email 格式驗證
+    for station in data:
+        station['sna'] = station['sna'].replace("YouBike2.0_", "")
 
-# 模擬數據庫 -> 可以換成數據庫連線:-> 變成其他形式
-users_db = [{
-  "id": 41000,
-  "name": "Zeny",
-  "email": "playboy@example.com"
-},
-{
-  "id": 41001,
-  "name": "Tony",
-  "email": "fuckboy@example.com"
-},
-{
-  "id": 41002,
-  "name": "Tony",
-  "email": "fuckboy@example.com"
-}
-]
+    df = pd.DataFrame(data)
+    df = df.sort_values("infoTime").query(f"infoTime >= '{datetime.now()-timedelta(days=1)}'")
+    return data
 
-@app.post("/users/")
-async def create_user(user: User):
-    # 確保用戶 ID 唯一
-    for u in users_db:
-        if u.id == user.id:
-            raise HTTPException(status_code=400, detail="User ID already exists")
-    users_db.append(user)
-    return user
+@app.get("/time")
+def get_time():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: int):
-    for user in users_db:
-        if user.get("id") == user_id:
-            return f'user name of user_id: {user.get("id")} is {user.get("name")}'
-    raise HTTPException(status_code=404, detail="User not found")
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
-@app.put("/users/{user_id}")
-async def update_user(user_id: int, updated_user: User):
-    for i, user in enumerate(users_db):
-        if user.get("id") == user_id:
-            users_db[i] = updated_user
-            return updated_user
-    raise HTTPException(status_code=404, detail="User not found")
-
-@app.delete("/users/{user_id}")
-async def delete_user(user_id: int):
-    for i, user in enumerate(users_db):
-        if user.id == user_id:
-            del users_db[i]
-            return {"detail": "User deleted"}
-    raise HTTPException(status_code=404, detail="User not found")
-
-@app.get("/wait/")
-async def wait_for_some_time():
-    await asyncio.sleep(3)  # 模擬長時間運行的任務
-    return {"message": "Finished waiting"}
